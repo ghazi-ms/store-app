f88d507124b62fbd01cdb4fa7349d3c1e4dbf80a
ghazi alsalah <ghazi.ms@hotmail.com>
2022-01-07 22:59:39 +0200

    added sql

---
+++ app/src/main/java/com/example/storeapp/DBHelper.kt
@@ -0,0 +1,122 @@
+package com.example.storeapp
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.ContactsContract
+import android.widget.*
+import android.widget.Toast
+import java.util.Arrays.toString
+
+
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+    // below is the method for creating a database by a sqlite query
+    override fun onCreate(db: SQLiteDatabase) {
+        // below is a sqlite query, where column names
+        // along with their data types is given
+       // val query = ("DROP TABLE " + TABLE_NAME )
+        val query = ("CREATE TABLE " + TABLE_NAME + " ("
+                + Phone_col + " INTEGER PRIMARY KEY, " +
+                NAME_COl + " TEXT," +
+                Last_name + " TEXT," +
+                User_password + " TEXT," +
+                USER_ADDRESS + " TEXT, " +
+                User_Name + " TEXT " +
+                ")")
+
+        // we are calling sqlite
+        // method for executing our query
+        db.execSQL(query)
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // this method is to check if table already exists
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    fun  CheckUser(user_name : String , password : String): Boolean {
+        val db = this.writableDatabase
+        val user:String=  db.rawQuery("SELECT User_name FROM " + TABLE_NAME + " WHERE User_name = " + user_name,null).toString()
+        val pass:String =db.rawQuery("SELECT User_password FROM " + TABLE_NAME + " WHERE User_name = " + user_name,null).toString()
+        db.close()
+        return true // user_name==user && password == pass
+
+    }
+    // This method is for adding data in our database
+    fun adduser(phone : Int , name : String , lastname : String , password : String , address : String , Username : String){
+
+
+        // below we are creating
+        // a content values variable
+        val values = ContentValues()
+
+        // we are inserting our values
+        // in the form of key-value pair
+        values.put(Phone_col, phone)
+        values.put(NAME_COl, name)
+        values.put(Last_name, lastname)
+        values.put(User_password, password)
+        values.put(USER_ADDRESS, address)
+        values.put(User_Name, Username)
+        // here we are creating a
+        // writable variable of
+        // our database as we want to
+        // insert value in our database
+        val db = this.writableDatabase
+
+        // all values are inserted into database
+        db.insert(TABLE_NAME, null, values)
+
+        // at last we are
+        // closing our database
+       db.close()
+    }
+
+    // below method is to get
+    // all data from our database
+    fun getName(): Cursor? {
+
+        // here we are creating a readable
+        // variable of our database
+        // as we want to read value from it
+        val db = this.readableDatabase
+
+        // below code returns a cursor to
+        // read data from the database
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+
+    }
+
+    companion object{
+        // here we have defined variables for our database
+
+        // below is variable for database name
+        private val DATABASE_NAME = "users"
+
+        // below is the variable for database version
+        private val DATABASE_VERSION = 1
+
+        // below is the variable for table name
+        val TABLE_NAME = "table_name"
+
+        // below is the variable for id column
+        val Phone_col = "phone"
+
+        // below is the variable for name column
+        val NAME_COl = "first_name"
+
+        // below is the variable for age column
+        val Last_name = "last_name"
+
+        val User_password ="User_password"
+
+        val USER_ADDRESS = "User_address"
+        val User_Name = "User_name"
+    }
+}
--- app/src/main/java/com/example/storeapp/MainActivity.kt
+++ app/src/main/java/com/example/storeapp/MainActivity.kt
@@ -4,22 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val username = findViewById<EditText>(R.id.User_name).text.toString()
+        val pass = findViewById<EditText>(R.id.User_password).text.toString()
         val SignUpBtn = findViewById<Button>(R.id.sign_up)
         val LogInBtn = findViewById<Button>(R.id.log_in)
+        val db = DBHelper(this, null)
+        LogInBtn.setOnClickListener {
 
-LogInBtn.setOnClickListener {
-    intent = Intent(this , user_page::class.java)
-    startActivity(intent)
-
-}
-        SignUpBtn.setOnClickListener {
-            intent =Intent(this, sign_up::class.java)
+//try to use key
+          //val res : Boolean=  db.CheckUser(username,pass)
+            //Toast.makeText(this,  " res " + res.toString(), Toast.LENGTH_LONG).show()
+            /*if (res) {
+                Toast.makeText(this,   " log in success", Toast.LENGTH_LONG).show()
+              //
+            }else  Toast.makeText(this,   " log in ERROR", Toast.LENGTH_LONG).show()*/
+            intent = Intent(this , user_page::class.java)
             startActivity(intent)
         }
+
+        SignUpBtn.setOnClickListener {
+            intent =Intent(this, sign_up::class.java)
+             startActivity(intent)
+        }
+
     }
 }
